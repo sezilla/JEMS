aa4eaa153c30a1776a2cc1a49182c6b1dfa0f76d
Kryx Loux <chthollyc@gmail.com>
2024-10-29 03:03:42 +0800

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -69,7 +69,7 @@ class AdminPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
 
-                managepackage::class,
+                // managepackage::class,
             ])
             ->authMiddleware([
                 Authenticate::class,
