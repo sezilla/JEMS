577d8c4ce72facd6d2e85258087057dc0f4d86c4
Kryx Loux <chthollyc@gmail.com>
2024-11-08 20:52:01 +0800

    Update task.blade.php

--- resources/views/filament/app/resources/project-resource/pages/task.blade.php
+++ resources/views/filament/app/resources/project-resource/pages/task.blade.php
@@ -1,18 +1,27 @@
 <x-filament::page>
     <h1>Task List from Trello</h1>
 
-    @if ($trelloCards)
+    @if ($trelloCards && count($trelloCards) > 0)
         @foreach ($trelloCards as $card)
             @php
                 $checklistItems = $this->getChecklistItems($card);
                 $dueDate = $card['due'] ? date('Y-m-d', strtotime($card['due'])) : 'N/A';
             @endphp
             
-            <x-checklist-table 
-                :cardName="$card['name']"
-                :dueDate="$dueDate"
-                :checklistItems="$checklistItems"
-                :cardId="$card['id']" />
+            <x-filament::section>
+                <h2>{{ $card['name'] }}</h2>
+                <p>Due Date: {{ $dueDate }}</p>
+                
+                @if ($checklistItems && count($checklistItems) > 0)
+                    <ul>
+                        @foreach ($checklistItems as $item)
+                            <li>{{ $item['name'] }} - {{ $item['state'] === 'complete' ? 'Completed' : 'Pending' }}</li>
+                        @endforeach
+                    </ul>
+                @else
+                    <p>No checklist items found.</p>
+                @endif
+            </x-filament::section>
         @endforeach
     @else
         <p>No cards found for the "Departments" list.</p>
