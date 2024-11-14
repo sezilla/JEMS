466f9d01de5b0a0fba493ccafb9b955c598de5e0
Kryx Loux <chthollyc@gmail.com>
2024-11-14 19:57:29 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,20 +38,12 @@
                                     </a>
                                 @else
                                     <a
-                                        href="{{ url('/app/edit-profile') }}"
+                                        href="{{ url('/app/edit-profile') }}" 
+                                        {{-- href="{{ url('/app/edit-profile') }}"  if user's first log in --}}
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF1493] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
                                         Log in
                                     </a>
-
-                                    {{-- @if (Route::has('register'))
-                                        <a
-                                            href="{{ route('register') }}"
-                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF1493] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                        >
-                                            Register
-                                        </a>
-                                    @endif --}}
                                 @endauth
                             </nav>
                         
