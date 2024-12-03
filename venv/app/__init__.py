a88493f04b16aae981e63220d62882bd2bd3cc44
Kryx Loux <chthollyc@gmail.com>
2024-12-03 19:14:02 +0800

    dafv

--- venv/app/__init__.py
+++
@@ -1,7 +0,0 @@
-# app/__init__.py
-
-from .database import engine, SessionLocal
-from .models import Package, Task, TaskPackage, Department, DepartmentTeam, Team, Project, ProjectTeam
-from .allocator import EventTeamAllocator
-from .schemas import ProjectAllocationRequest
-from .logging_config import logger
