1c78f8de475b8b067884ef91fb2867a774989d0c
Kryx Loux <chthollyc@gmail.com>
2024-12-06 18:09:24 +0800

    try python framework #3

--- venv/app/app.py
+++ venv/app/app.py
@@ -1,11 +1,12 @@
-from app import app
+import app
+from app.utils.ssh_tunnel import start_tunnel, stop_tunnel
+from app.db import initialize_database
 
 @app.on_event("startup")
 def startup_event():
-    from app.utils.ssh_tunnel import start_tunnel
     start_tunnel()
+    initialize_database()  # Initialize the database connection after starting the tunnel
 
 @app.on_event("shutdown")
 def shutdown_event():
-    from app.utils.ssh_tunnel import stop_tunnel
     stop_tunnel()
