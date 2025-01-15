74b277a04e28ab533083b5de823cdf2c3261d078
Kryx Loux <chthollyc@gmail.com>
2025-01-15 17:26:13 +0800

    Update trello-task.blade.php

--- resources/views/livewire/trello-task.blade.php
+++ resources/views/livewire/trello-task.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <x-filament::table :columns="$this->getTableColumns()" :records="$this->getTableData()" />
+    {{-- <x-filament::table :columns="$this->getTableColumns()" :records="$this->getTableData()" /> --}}
 </div>
