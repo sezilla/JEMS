ad7aa87fb50e0f7ed4bf85c6493cb5ad2d4b3a11
Sezilla <chthollyc@gmail.com>
2025-02-26 20:28:47 +0800

    hmm

--- app/Filament/Resources/TaskResource/Pages/EditTask.php
+++ app/Filament/Resources/TaskResource/Pages/EditTask.php
@@ -10,6 +10,11 @@ class EditTask extends EditRecord
 {
     protected static string $resource = TaskResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
     protected function getHeaderActions(): array
     {
         return [
