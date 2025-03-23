b02c7372e523c91678aa203eac15e879587b9022
sezilla <chthollyc@gmail.com>
2025-03-23 15:27:38 +0800

    mailing mailer

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 
 Route::get('/', function () {
@@ -11,4 +12,18 @@ Route::get('/', function () {
 Route::post('/projects/allocate-teams', [ProjectController::class, 'allocateTeams']);
 Route::get('/projects/history', [ProjectController::class, 'getProjectHistory']);
 
+Route::middleware('auth')->group(function () {
+    Route::get('/email/verify', function () {
+        return view('auth.verify-email');
+    })->name('verification.notice');
 
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+        return redirect('/home');
+    })->middleware(['signed'])->name('verification.verify');
+
+    Route::post('/email/resend', function () {
+        request()->user()->sendEmailVerificationNotification();
+        return back()->with('message', 'Verification link sent!');
+    })->middleware(['throttle:6,1'])->name('verification.send');
+});
