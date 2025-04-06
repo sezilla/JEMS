7b2a51c73d2b34ebdbb68f9ad9b1fc255d8b18e9
axrylle <135582153+axrylle@users.noreply.github.com>
2025-04-07 07:34:38 +0800

    updated dashboard

--- app/Filament/Widgets/UsersLineChart.php
+++ app/Filament/Widgets/UsersLineChart.php
@@ -3,11 +3,14 @@
 namespace App\Filament\Widgets;
 
 use Filament\Widgets\ChartWidget;
+use App\Models\User; // Make sure to import the User model
+use Carbon\Carbon;
 use App\Models\Department;
 
-class EmployeeStats extends ChartWidget
+class UsersLineChart extends ChartWidget
 {
-    protected static ?string $heading = 'Employee Per Department';
+//     
+protected static ?string $heading = 'Employee Per Department';
 
     protected function getData(): array
     {
