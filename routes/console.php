a0c655467fd96aa95f9643f90c3b3cb1f5ae69ff
Karl <chthollyc@gmail.com>
2025-05-18 21:52:40 +0800

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,16 @@
 <?php
 
+use App\Models\Project;
+use App\Models\UserTask;
 use App\Jobs\CheckTaskDue;
+use App\Services\TrelloTask;
+use App\Services\ProjectTaskService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::job(CheckTaskDue::class)->dailyAt('00:00');
+Schedule::job(new CheckTaskDue(new ProjectTaskService(new Project, new TrelloTask, new UserTask)))->dailyAt('00:00');
